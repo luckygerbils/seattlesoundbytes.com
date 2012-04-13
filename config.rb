27d23287b713c9d5eb9d8feaaaba55274c53c327
Sean Anastasi <sean.anastasi@gmail.com>
2012-04-13 05:23:23 +0000

    Changes

---
+++ config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+Encoding.default_external = 'utf-8'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "www/css"
+sass_dir = "site/css"
+images_dir = "www/img"
+http_images_path = "/img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
