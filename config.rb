f598007f52a62a526c815d838ffe04f1258e6b3d
Anastasi <sanasta@e80688df6110.ant.amazon.com>
2012-04-15 22:10:56 -0700

    Set up minification.

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@
 # External file encoding should be UTF-8
 Encoding.default_external = 'utf-8'
 
+# Output directory.
 set :build_dir,   'www'
 
 activate :directory_indexes
@@ -13,10 +14,6 @@ activate :directory_indexes
 ###
 compass_config do |config|
     config.http_path = "/"
-    #config.sass_dir = "source/css"
-    #config.images_dir = "source/img"
-    #config.http_images_path = "/img"
-    #config.javascripts_dir = "js"
 end
 
 ###
@@ -24,19 +21,13 @@ end
 ###
 set :haml, { :attr_wrapper => "\"" } 
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
 # Build-specific configuration
 configure :build do
+  # Minify CSS.and Javascript
   activate :minify_css
-  
+  activate :minify_javascript
+  # Compress images.
+  activate :smusher
+  # Let HTML output be ugly.
   set :haml, { :ugly => true }
 end
